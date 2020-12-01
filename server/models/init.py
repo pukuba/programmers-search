e1c272bd73d243267c23f8fc82b75b2d3547ad4d
pukuba <skatmddnjs0604@gmail.com>
2020-12-02 04:14:50 +0900

    Redefine csv

--- server/models/init.py
+++ server/models/init.py
@@ -6,20 +6,33 @@ import csv
 f = open('db.csv', 'w', encoding='utf-8', newline='')
 wr = csv.writer(f)
 
-wr.writerow(["title","lv","url","tag","id"])
+wr.writerow(["title","lv","url","tag","id","description"])
 cnt = 0
+
+def description_parse(x):
+    webpage = requests.get(x)
+    soup = BeautifulSoup(webpage.text,"html.parser")
+    a = soup.find_all(attrs={'class':'guide-section'})
+    a[0]['style']="width: calc(50% - 12px);"
+    # for item in a:
+    #     item['style']="width: calc(40% - 12px);"
+    #     break
+    return a[0]
+
 def parse(x):
     global cnt
     title = x.h4.get_text()
     url = "https://programmers.co.kr"+x.attrs['href']
     level = str(x.parent.parent.find(attrs={'class':'card-algorithm'}))
     tag = x.find(attrs={'class':'level'}).get_text()
-
+    
+    
     
     # print("level:" + level[33:34])
     # print(title)
     # print(url)
-    wr.writerow([title,level[33:34], url,tag, cnt])
+    
+    wr.writerow([title,level[33:34], url,tag, cnt, description_parse(url)])
     cnt += 1
 
 for page in pages:
