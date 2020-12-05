4d2491f2ecbb12b1911c29e40fcd0d953c64bb2d
pukuba <skatmddnjs0604@gmail.com>
2020-12-05 09:05:49 +0900

    use mongo pk

--- server/resolvers/Type.js
+++ server/resolvers/Type.js
@@ -1,3 +1,11 @@
 module.exports = {
-
+    Post: {
+        id: parent => parent._id
+    },
+    Comment: {
+        id: parent => parent._id
+    },
+    Problem: {
+        id: parent => parent._id
+    }
 }
