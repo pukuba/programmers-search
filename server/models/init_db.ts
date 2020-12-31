ac405a39757dcc2f5c754fe5e8cd6acbd7f47390
SeungWon <20sunrin041@sunrint.hs.kr>
2021-01-01 02:17:56 +0900

    fixed path error

--- server/models/init_db.ts
+++ server/models/init_db.ts
@@ -1,9 +1,7 @@
-const dotenv = require('dotenv')
-const path = require('path')
-
-const { MongoClient } = require('mongodb')
-
-const csv = require('csvtojson')
+import dotenv from 'dotenv'
+import path from 'path'
+import { MongoClient } from 'mongodb'
+import csv from 'csvtojson'
 const csvFilePath = "./db.csv"
 
 dotenv.config({ path: path.join(__dirname, '../.env') })
@@ -12,7 +10,7 @@ const init = async () => {
     const jsonArray = await csv().fromFile(csvFilePath)
 
     const client = await MongoClient.connect(
-        process.env.DB_HOST1, {
+        String(process.env.DB_HOST1), {
         useUnifiedTopology: true,
         useNewUrlParser: true
     }
